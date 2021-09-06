1cd6e5eeac1af4a8171042e95815efe2f2012ef1
Rohan157 <sp18-bcs-040@cuilahore.edu.pk>
2021-09-06 22:39:56 +0500

    First Commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { readdirSync } from "fs";
+import cors from "cors";
+import mongoose from "mongoose";
+require("dotenv").config();
+const morgan = require("morgan");
+
+const app = express();
+
+//db connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DataBase Connected"))
+  .catch((err) => console.log("DB Connection Error:", err));
+//morgan is use here to show routes in console. it should only be used in development
+//Remeber to remove it from here once development is done.
+//Regards Rohan
+//middleware
+//cors is used to run frontend and backend simultaneously otherwise their linking can cause error
+app.use(cors());
+app.use(morgan("dev"));
+//route middleware
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
