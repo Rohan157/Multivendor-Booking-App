192e0ce9647584f0c68180f9d3f06f8fe9fd858d
Rohan157 <sp18-bcs-040@cuilahore.edu.pk>
2021-09-18 03:25:42 +0500

    Hotels display on HomeScreen Done

--- server/routes/hotel.js
+++ server/routes/hotel.js
@@ -8,9 +8,13 @@ const router = express.Router();
 import { requireSignin } from "../middlewares";
 
 //COntrollers
-import { create } from "../controllers/hotel";
+import { create, hotels, image } from "../controllers/hotel";
 
 //Formidable here is necessary for the purpose of conversion of Form data
 router.post("/create-hotel", requireSignin, formidable(), create);
+//TO get the hotels so we can show them on the Home screen, we use get request
+router.get("/hotels", hotels);
+//to get img
+router.get("/hotel/image/:hotelId", image);
 
 module.exports = router;
